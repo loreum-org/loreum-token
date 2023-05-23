fc1306e53ffd6825b5db5e98e370572cb18288ef
Chad Lynch <chyad@hotmail.com>
2023-05-23 01:48:07 -0400

    fix: repo deps for hardhat

--- src/LoreumToken.sol
+++ src/LoreumToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-only
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
 import { ERC20 } from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import { ERC20Permit } from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
