c8fff9a1916c05b463188eb45ee8d8f70643711d
Chad Lynch <chyad@hotmail.com>
2023-05-23 02:17:57 -0400

    fix: hre solc v0.8.18

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -45,7 +45,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.20",
+        version: "0.8.18",
         settings: { optimizer: { enabled: true, runs: 88888 } },
       },
       {
