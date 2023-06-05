02b5cc45a19b115c2e9d85f73548884cbd070a14
pseudonaut <jquarnstrom@yahoo.com>
2023-06-05 15:47:47 -0700

    Remove ASCII

--- src/LoreumToken.sol
+++ src/LoreumToken.sol
@@ -3,21 +3,7 @@ pragma solidity ^0.8.18;
 
 import { ERC20 } from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import { ERC20Permit } from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import { Ownable } from "openzeppelin-contracts/contracts/access/Ownable.sol";
-
-
-//   ▄           ▄▄▄▄▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄▄▄ ▄         ▄ ▄▄       ▄▄ 
-//  ▐░▌         ▐░░░░░░░░░░░▐░░░░░░░░░░░▐░░░░░░░░░░░▐░▌       ▐░▐░░▌     ▐░░▌
-//  ▐░▌         ▐░█▀▀▀▀▀▀▀█░▐░█▀▀▀▀▀▀▀█░▐░█▀▀▀▀▀▀▀▀▀▐░▌       ▐░▐░▌░▌   ▐░▐░▌
-//  ▐░▌         ▐░▌       ▐░▐░▌       ▐░▐░▌         ▐░▌       ▐░▐░▌▐░▌ ▐░▌▐░▌
-//  ▐░▌         ▐░▌       ▐░▐░█▄▄▄▄▄▄▄█░▐░█▄▄▄▄▄▄▄▄▄▐░▌       ▐░▐░▌ ▐░▐░▌ ▐░▌
-//  ▐░▌         ▐░▌       ▐░▐░░░░░░░░░░░▐░░░░░░░░░░░▐░▌       ▐░▐░▌  ▐░▌  ▐░▌
-//  ▐░▌         ▐░▌       ▐░▐░█▀▀▀▀█░█▀▀▐░█▀▀▀▀▀▀▀▀▀▐░▌       ▐░▐░▌   ▀   ▐░▌
-//  ▐░▌         ▐░▌       ▐░▐░▌     ▐░▌ ▐░▌         ▐░▌       ▐░▐░▌       ▐░▌
-//  ▐░█▄▄▄▄▄▄▄▄▄▐░█▄▄▄▄▄▄▄█░▐░▌      ▐░▌▐░█▄▄▄▄▄▄▄▄▄▐░█▄▄▄▄▄▄▄█░▐░▌       ▐░▌
-//  ▐░░░░░░░░░░░▐░░░░░░░░░░░▐░▌       ▐░▐░░░░░░░░░░░▐░░░░░░░░░░░▐░▌       ▐░▌
-//   ▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀ ▀         ▀ ▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀ ▀         ▀ 
-//                                                                           
+import { Ownable } from "openzeppelin-contracts/contracts/access/Ownable.sol";                                       
 
 contract LoreumToken is ERC20, ERC20Permit, Ownable {
     
